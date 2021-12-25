39c523b975cb9333f27f894bb9725c4268263fc5
Stanislav <skonashuk@list.ru>
2021-12-26 02:07:44 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
