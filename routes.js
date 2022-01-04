f3dbdae15f04df7f42da2a0c95d68c88f2827d77
Stanislav <skonashuk@list.ru>
2022-01-04 20:30:04 +0300

    Add registration and refresh token routes

--- routes.js
+++ routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const rootRouter = express.Router();
-const projects = require('./projects');
-const auth = require('./auth');
+const projectsController = require('./controller/projectsConroller');
+const authController = require('./controller/authController');
 
-rootRouter.use('/projects', projects);
-rootRouter.use('/login', auth);
+rootRouter.get('/projects', projectsController.getProjects);
+rootRouter.post('/login', authController.login);
+rootRouter.post('/registration', authController.registration);
+rootRouter.get('/refresh_token', authController.refreshToken);
+rootRouter.delete('/refresh_token', authController.deleteRefreshToken);
 
 module.exports = rootRouter;
