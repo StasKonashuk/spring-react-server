e0ba2b88357ee100d1dcd03521e720eec2f74e5e
Stanislav <skonashuk@list.ru>
2022-01-04 20:27:53 +0300

    Add cors configure and cookieParser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const rootRouter = require('./routes');
 
 const PORT = process.env.PORT || 5000;
@@ -8,7 +9,14 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use(cookieParser());
+
 app.use('/', rootRouter);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
