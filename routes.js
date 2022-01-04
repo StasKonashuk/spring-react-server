ed9b36b883ca61605167ccbac92376ab0c76043b
Stanislav <skonashuk@list.ru>
2022-01-04 20:45:29 +0300

    Add protect by JWT to projects endpoint

--- routes.js
+++ routes.js
@@ -3,8 +3,13 @@ const express = require('express');
 const rootRouter = express.Router();
 const projectsController = require('./controller/projectsConroller');
 const authController = require('./controller/authController');
+const authentificateToken = require('./middleware/authorizaton');
 
-rootRouter.get('/projects', projectsController.getProjects);
+rootRouter.get(
+  '/projects',
+  authentificateToken,
+  projectsController.getProjects
+);
 rootRouter.post('/login', authController.login);
 rootRouter.post('/registration', authController.registration);
 rootRouter.get('/refresh_token', authController.refreshToken);
