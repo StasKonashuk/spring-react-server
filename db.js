b88a7cbdd661947f18e1aa377458413545f2674c
Stanislav <skonashuk@list.ru>
2022-01-05 15:34:23 +0300

    Add db for production

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const Pool = require('pg').Pool;
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.DB_NAME}`;
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
