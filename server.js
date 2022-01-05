97c4600b0aee1bf6d072fb89d38bfdded7051b9c
Stanislav <skonashuk@list.ru>
2022-01-05 22:41:17 +0300

    Fix server

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
 const rootRouter = require('./routes');
 
 const PORT = process.env.PORT || 5000;
@@ -14,22 +13,11 @@ app.use(
   cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
-    exposedHeaders: ['set-cookie'],
   })
 );
+
+app.set('trust proxy', 1);
 app.use(cookieParser());
-app.enable('trust proxy');
-app.use(
-  session({
-    secret: 'somesecret',
-    key: 'sid',
-    proxy: true,
-    cookie: {
-      secure: true,
-      maxAge: 5184000000,
-    },
-  })
-);
 
 app.use('/', rootRouter);
 
