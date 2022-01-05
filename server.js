6d339f0c689d59db4c421bcdbc1402264dda2996
Stanislav <skonashuk@list.ru>
2022-01-05 21:08:14 +0300

    Add express-session

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const rootRouter = require('./routes');
 
 const PORT = process.env.PORT || 5000;
@@ -16,6 +17,19 @@ app.use(
   })
 );
 app.use(cookieParser());
+app.use(
+  session({
+    name: 'random_session',
+    secret: 'yryGGeugidx34otGDuSF5sD9R8g0Gü3r8',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      path: '/',
+      secure: true,
+      httpOnly: true,
+    },
+  })
+);
 
 app.use('/', rootRouter);
 
