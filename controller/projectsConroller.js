1de8017131bf5133e86cddd9080f393478b82ca6
Stanislav <skonashuk@list.ru>
2022-01-08 17:54:08 +0300

    Add orm to projectsController

--- controller/projectsConroller.js
+++ controller/projectsConroller.js
@@ -1,15 +1,17 @@
-const db = require('../db');
+const Projects = require('../models/Projects');
+const AtticProjects = require('../models/AtticProjects');
+const FooterProjects = require('../models/FooterProjects');
 
 class ProjectsController {
   async getProjects(req, res) {
     try {
       const projectsInfo = {};
-      const projects = await db.query('SELECT * FROM projects');
-      const atticProjects = await db.query('SELECT * FROM attic_projects');
-      const footerProjects = await db.query('SELECT * FROM footer_projects');
-      projectsInfo.projects = projects.rows;
-      projectsInfo.atticProjects = atticProjects.rows;
-      projectsInfo.footerProjects = footerProjects.rows;
+      const projects = await Projects.findAll();
+      const atticProjects = await AtticProjects.findAll();
+      const footerProjects = await FooterProjects.findAll();
+      projectsInfo.projects = projects;
+      projectsInfo.atticProjects = atticProjects;
+      projectsInfo.footerProjects = footerProjects;
       res.json(projectsInfo);
     } catch {
       res.status(500).send();
