34e9e4fefc1f5d9238ebb16b5b1b7cd64afebdf9
Stanislav <skonashuk@list.ru>
2022-01-08 17:48:30 +0300

    Create connection to database

--- db.js
+++ db.js
@@ -1,14 +1,27 @@
-const Pool = require('pg').Pool;
+/* const Pool = require('pg').Pool;
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.DB_NAME}`;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
+ */
+const { Sequelize } = require('sequelize');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const db = new Sequelize(
+  isProduction
+    ? process.env.DATABASE_URL
+    : `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+);
+
+module.exports = db;
