9ab7fba0f75c9426be312ff96a1f083c25e6f775
Stanislav <skonashuk@list.ru>
2022-01-08 17:54:31 +0300

    Add orm to authController

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,8 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const schema = require('../validators/schema');
-const validate = require('../middleware/validate');
-const pool = require('../db');
+const validate = require('../validators/validate');
+const Users = require('../models/Users');
 const jwtTokens = require('../utils/jwt-helpers');
 
 class AuthController {
@@ -15,23 +15,26 @@ class AuthController {
         { userName, password, repeatPassword, firstName, lastName, age },
         schema
       );
+
       if (validateResult.error) {
         return res.status(400).json({ error: validateResult.detail });
       }
+
       const hashPassword = await bcrypt.hash(password, 8);
-      const candidate = await pool.query(
-        `SELECT * FROM users
-      WHERE userName = $1`,
-        [userName]
-      );
-      if (candidate.rows.length > 0) {
+
+      const candidate = await Users.findOne({ where: { user_name: userName } });
+      if (candidate) {
         return res.status(401).json({ message: 'User already registered' });
       }
-      await pool.query(
-        `INSERT INTO users (userName, password, firstName, lastName, age)
-          VALUES ($1, $2, $3, $4, $5)`,
-        [userName, hashPassword, firstName, lastName, age]
-      );
+
+      await Users.create({
+        user_name: userName,
+        password: hashPassword,
+        first_name: firstName,
+        last_name: lastName,
+        age,
+      });
+
       res.status(200).json({ message: 'User registered successfully' });
     } catch (error) {
       res.status(401).json({ error: error.message });
@@ -41,22 +44,18 @@ class AuthController {
   async login(req, res) {
     try {
       const { userName, password } = req.body;
-      const user = await pool.query(
-        `SELECT * FROM users
-      WHERE userName = $1`,
-        [userName]
-      );
-      if (user.rows.length === 0) {
+
+      const user = await Users.findOne({ where: { user_name: userName } });
+      if (!user) {
         return res.status(401).json({ message: 'Invalid user name' });
       }
-      const validPassword = await bcrypt.compare(
-        password,
-        user.rows[0].password
-      );
+
+      const validPassword = await bcrypt.compare(password, user.password);
       if (!validPassword) {
         return res.status(401).json({ message: 'Invalid password' });
       }
-      const tokens = jwtTokens(user.rows[0]);
+
+      const tokens = jwtTokens(user);
       res.cookie('refresh_token', tokens.refreshToken, { httpOnly: true });
       res.status(200).json(tokens);
     } catch (error) {
@@ -67,9 +66,11 @@ class AuthController {
   refreshToken(req, res) {
     try {
       const refreshToken = req.cookies.refresh_token;
+
       if (refreshToken === null) {
         return res.status(401).json({ error: 'Null refresh token' });
       }
+
       jwt.verify(
         refreshToken,
         process.env.JWT_REFRESH_SECRET,
