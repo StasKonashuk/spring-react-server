16fab65da4f1f314b443582b671f85d9fddf2013
Stanislav <skonashuk@list.ru>
2022-01-09 20:53:50 +0300

    Fix refresh route

--- routes.js
+++ routes.js
@@ -12,7 +12,7 @@ rootRouter.get(
 );
 rootRouter.post('/login', authController.login);
 rootRouter.post('/registration', authController.registration);
-rootRouter.get('/refresh-token', authController.refreshToken);
+rootRouter.post('/refresh-token', authController.refreshToken);
 rootRouter.delete('/refresh-token', authController.deleteRefreshToken);
 
 module.exports = rootRouter;
