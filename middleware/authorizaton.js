ab1ba8bda09dd9da4dfdbd9289fbadfdb3357e81
Stanislav <skonashuk@list.ru>
2022-01-10 11:43:58 +0300

    Fix authorization middleware

--- middleware/authorizaton.js
+++ middleware/authorizaton.js
@@ -1,14 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 function authentificateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token === null) return res.status(401).json({ error: 'Null token' });
-  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (error, user) => {
-    if (error) return res.status(403).json({ error: error.message });
-    req.user = user;
-    next();
-  });
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token === null) return res.status(401).json({ error: 'Null token' });
+    jwt.verify(token, process.env.JWT_ACCESS_SECRET, (error, user) => {
+      if (error) return res.status(403).json({ error: error.message });
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 module.exports = authentificateToken;
